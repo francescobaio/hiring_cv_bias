c236336680e573bac34bfb781dea5e84b0c1ec1b
Leonardo Petrilli <leonardorossi890@gmail.com>
2025-03-14 13:42:57 +0100

    add: load data

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
-import pandas as pd 
+import polars as pl
 
 
-df = pd.read_csv('direct_matching_20240213.csv', sep=';')
-print(df[160:170])
+def load_and_process_data():
+    df_skills = pl.read_csv('data/Adecco_csv_parsed_data/parsed_data.csv', separator=';').rename({'CANDIDATE_ID':'cand_id'})
+    df_info = pl.read_csv('data/Adecco_dataset/reverse_matching_20240213.csv', separator=';')
+    df_info = df_info.with_columns(df_info.select(pl.col("cand_id").str.replace_all(',','',literal=True).cast(pl.Int64).alias('cand_id')))
+    df_total = df_skills.join(df_info, on='cand_id')
+    
+    return df_skills, df_info, df_total
+
+
 
 
 
