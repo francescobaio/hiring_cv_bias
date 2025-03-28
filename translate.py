d703535524c4c0b063fc0a501957a7f3dafc3532
francescobaio <francesco.baiocchi16@gmail.com>
2025-03-28 13:50:41 +0100

    add: translate file for CVs with Google API, some gender_analysis adjustments

---
+++ translate.py
@@ -0,0 +1,39 @@
+from utils import load_data
+from config import CANDIDATE_CVS_PATH
+from google.cloud import translate_v2 as translate
+import polars as pl
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
+    "magnetic-market-455110-t2-0dc68481bf13.json"
+)
+
+
+def clean_cv_text(cv_text: str) -> str:
+    cv_text = cv_text.replace("\n", " ")
+    cv_text = cv_text.replace("CV anonimizzato:", "")
+    cv_text = cv_text.replace('"""', "")
+    cv_text = cv_text.sub(" +", " ", cv_text)
+    cv_text = cv_text.replace(" ,", ",")
+    cv_text = cv_text.lower()
+    return cv_text
+
+
+def translate_text(text, target_language="en"):
+    text = clean_cv_text(text)
+    client = translate.Client()
+    result = client.translate(text, target_language=target_language)
+    return result["translatedText"]
+
+
+if __name__ == "main":
+    cv_data = load_data(CANDIDATE_CVS_PATH)
+    cv_df_eng = cv_data.with_columns(
+        pl.col("CV_text_anon")
+        .map_elements(
+            lambda x: translate_text(x, target_language="en"), return_dtype=pl.String
+        )
+        .alias("Translated_CV")
+    )
+    cv_df_eng.write_csv(f"Candidate_CVs_translated.csv", separator=";")
+    print("Translations completed e saved in Candidate_CVs_translated.csv")
