9ae298a33a196fd775c1b181e5caa62d02ec3261
francescobaio <francesco.baiocchi16@gmail.com>
2025-07-12 19:29:00 +0200

    Small fixes and general refactor

--- hiring_cv_bias/bias_detection/rule_based/data.py
+++ hiring_cv_bias/bias_detection/rule_based/data.py
@@ -7,8 +7,9 @@ def add_demographic_info(
     cv_df: pl.DataFrame,
     candidates_df: pl.DataFrame,
 ) -> pl.DataFrame:
+    candidates_df = candidates_df.filter(pl.col("Gender").is_in(["Male", "Female"]))
     enriched_cv_df = cv_df.join(
-        candidates_df.select(["CANDIDATE_ID", "Gender", "Location", "Age_bucket"]),
+        candidates_df.select(["CANDIDATE_ID", "Gender", "Location"]),
         on="CANDIDATE_ID",
         how="inner",
     ).with_columns(
@@ -20,11 +21,3 @@ def add_demographic_info(
     )
 
     return enriched_cv_df
-
-
-def filter_unknown_and_other_rows(
-    cv_df: pl.DataFrame,
-) -> pl.DataFrame:
-    cv_df = cv_df.filter(pl.col("Gender").is_in(["Male", "Female"]))
-    cv_df = cv_df.filter(pl.col("Age_bucket").is_in(["25-34", "45-54", "55-74"]))
-    return cv_df
--- hiring_cv_bias/bias_detection/rule_based/patterns.py
+++ hiring_cv_bias/bias_detection/rule_based/patterns.py
@@ -21,33 +21,8 @@ driver_license_pattern_eng = re.compile(
     re.IGNORECASE | re.UNICODE,
 )
 
-# driver_license_pattern_it = re.compile(
-#     r"(?:"
-#     r"\bpatent[ei]\b(?:\s*(?:di\s*(?:guida|categoria))?\s*[:\-]?\s*\w+)?|"
-#     r"\bautomunit[oa](?:\/[oa])?\b|"
-#     r"\b(?:patente(?:\s+di\s+guida)?|categoria|cat\.?|driving\s+licen[cs]e)\s*[:\-]?\s*[A-E][1-9]?\b"
-#     r"\bmunit[oa]\b"
-#     r")",
-#     re.IGNORECASE | re.UNICODE,
-# )
 
-# ----------------------------------------------------------
-
-# LANGUAGE_REGEXES = {
-#     lang: re.compile(
-#         rf"""
-#         \b(?:{"|".join(map(re.escape, variants))})\b
-#         |madrelingua\s+(?:{"|".join(map(re.escape, variants))})
-#         |lingua\s+(?:{"|".join(map(re.escape, variants))})
-#         |conoscenza\s+(?:della\s+)?(?:lingua\s+)?(?:{"|".join(map(re.escape, variants))})
-#         |certificazione\s+(?:di\s+)?(?:{"|".join(map(re.escape, variants))})
-#         |(?:{"|".join(map(re.escape, variants))})\s+(?:parlata|scritta|orale)
-#         |(?:{"|".join(map(re.escape, variants))})\s*(?:A1|A2|B1|B2|C1|C2|madrelingua)
-#         """,
-#         re.IGNORECASE | re.VERBOSE,
-#     )
-#     for lang, variants in LANGUAGE_VARIANTS.items()
-# }
+# ---------------------------------------------------
 
 LANGUAGE_VARIANTS = {}
 
@@ -69,6 +44,7 @@ for lang in pycountry.languages:
         variants.add(native)
 
     LANGUAGE_VARIANTS[code] = sorted(variants)
+
 
 LANGUAGE_REGEXES_EN = {
     lang: re.compile(
@@ -92,6 +68,9 @@ languages_pattern_eng = re.compile(
     "|".join(f"({pat.pattern})" for pat in LANGUAGE_REGEXES_EN.values()),
     re.IGNORECASE | re.VERBOSE,
 )
+
+
+# ---------------------------------------------------
 
 
 jobs = pl.read_csv(JOBS_PATH)["preferredLabel"].to_list()
