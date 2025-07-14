36c32dc8c2261d21bc0c0ecb5280be8f8dc682eb
francescobaio <francesco.baiocchi16@gmail.com>
2025-07-14 16:23:06 +0200

    Added notebooks with results

--- hiring_cv_bias/utils.py
+++ hiring_cv_bias/utils.py
@@ -1,8 +1,14 @@
+from typing import Dict, List
+
 import polars as pl
 
 
 def load_data(filepath: str) -> pl.DataFrame:
     return pl.read_csv(filepath, separator=";")
+
+
+def load_excel_sheets(path: str, sheets: List[str]) -> Dict[str, pl.DataFrame]:
+    return {sheet: pl.read_excel(path, sheet_name=sheet) for sheet in sheets}
 
 
 def filter_unknown_and_other_rows(
